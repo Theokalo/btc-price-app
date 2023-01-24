821895866a15d1418235ed8c97d51faec3e9198e
theokalo <theodoros.kalogeropoulos@epione.care>
2023-01-24 18:20:16 +0100

    import MethodTypes and created constants for the urls

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,18 +1,22 @@
 import { request } from "../services/requests";
+import { MethodTypes } from "../services/method_types";
+
+const currentPriceURL = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+const currenciesURL = 'https://api.getgeoapi.com/v2/currency/convert?api_key=a6d2b23eb2ae0f35e5b6aa0bff7541be101bccb6&from=EUR&amount=';
 
 export const getBTCPrice = async (currency:string) => {
-   const response = await request('https://api.coindesk.com/v1/bpi/currentprice.json','GET');
+   const response = await request(currentPriceURL, MethodTypes.get);
    return {price: convertBTCToCurrency(response?.bpi?.EUR?.rate_float, currency), timestamp: response?.time?.updated}
 }
 
 const convertBTCToCurrency = async (amount:number, currency:string) => {
-    const response = await request(`https://api.getgeoapi.com/v2/currency/convert?api_key=a6d2b23eb2ae0f35e5b6aa0bff7541be101bccb6&from=EUR&amount=${amount}&format=json`,'GET');
+    const response = await request(`${currenciesURL}${amount}&format=json`, MethodTypes.get);
         return response?.rates?.[currency]?.rate_for_amount
 }
 
 export const getAllCurrencies = async () => {
     let currencies:Array<Record<string, string>> = [];
-    const response = await request(`https://api.getgeoapi.com/v2/currency/convert?api_key=a6d2b23eb2ae0f35e5b6aa0bff7541be101bccb6&from=EUR&amount=10&format=json`,'GET');
+    const response = await request(`${currenciesURL}10&format=json`, MethodTypes.get);
     if(!response?.rates)
         return [{key:'0' , value:'EUR'}];
     else {
