3e1506d026fda1deae63c41dc74da3c201f94982
theokalo <theodoros.kalogeropoulos@epione.care>
2023-01-24 12:15:57 +0100

    helper to get the necessary responses from  the apis

---
+++ src/helpers/index.ts
@@ -0,0 +1,21 @@
+import { request } from "../services/requests";
+
+export const getBTCPrice = async (currency:string) => {
+   const response = await request('https://api.coindesk.com/v1/bpi/currentprice.json','GET');
+   return {price: convertBTCToCurrency(response?.bpi?.EUR?.rate_float, currency), timestamp: response?.time?.updated}
+}
+
+const convertBTCToCurrency = async (amount:number, currency:string) => {
+    const response = await request(`https://api.getgeoapi.com/v2/currency/convert?api_key=a6d2b23eb2ae0f35e5b6aa0bff7541be101bccb6&from=EUR&amount=${amount}&format=json`,'GET');
+    return response?.rates?.[currency]?.rate_for_amount
+}
+
+export const getAllCurrencies = async () => {
+    let currencies:Array<Record<string, string>> = [];
+    const response = await request(`https://api.getgeoapi.com/v2/currency/convert?api_key=a6d2b23eb2ae0f35e5b6aa0bff7541be101bccb6&from=EUR&amount=10&format=json`,'GET');
+    Object.keys(response?.rates).forEach(function(key, index) {
+        currencies.push({key, value:key})
+    });
+
+    return currencies;
+}
