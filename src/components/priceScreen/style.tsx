faec516de3fc3dd3d9940bc21b2934321df8a910
theokalo <theodoros.kalogeropoulos@epione.care>
2023-01-24 12:18:00 +0100

    style file for the priceScreen component

---
+++ src/components/priceScreen/style.tsx
@@ -0,0 +1,32 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#333333",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    priceTitle: {
+        fontSize: 20,
+        color: '#fff'
+    },
+    price: {
+        fontSize: 15,
+        fontWeight: '700',
+        color: '#F7931A',
+    },
+    timeTitle: {
+        fontSize: 20,
+        color: '#fff'
+    },
+    time: {
+        fontSize: 15,
+        fontWeight: '700',
+        color: '#F7931A',
+    },
+    logo: {
+      width: 66,
+      height: 66,
+    },
+  });
