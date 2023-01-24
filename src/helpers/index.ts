9c8da79416f352eb4f19258572a3891effdc5985
theokalo <theodoros.kalogeropoulos@epione.care>
2023-01-24 15:31:51 +0100

    check if there is response

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -7,15 +7,19 @@ export const getBTCPrice = async (currency:string) => {
 
 const convertBTCToCurrency = async (amount:number, currency:string) => {
     const response = await request(`https://api.getgeoapi.com/v2/currency/convert?api_key=a6d2b23eb2ae0f35e5b6aa0bff7541be101bccb6&from=EUR&amount=${amount}&format=json`,'GET');
-    return response?.rates?.[currency]?.rate_for_amount
+        return response?.rates?.[currency]?.rate_for_amount
 }
 
 export const getAllCurrencies = async () => {
     let currencies:Array<Record<string, string>> = [];
     const response = await request(`https://api.getgeoapi.com/v2/currency/convert?api_key=a6d2b23eb2ae0f35e5b6aa0bff7541be101bccb6&from=EUR&amount=10&format=json`,'GET');
-    Object.keys(response?.rates).forEach( (key, index) => {
-        currencies.push({key:index.toString() , value:key})
-    });
+    if(!response?.rates)
+        return [{key:'0' , value:'EUR'}];
+    else {
+        Object.keys(response?.rates).forEach( (key, index) => {
+            currencies.push({key:index.toString() , value:key})
+        });
 
-    return currencies;
+        return currencies;
+    }
 }
